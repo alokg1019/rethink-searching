2fb8fc75c5007ae8c7c11b27e489cda6ebff1fac
Alok Goyal <alokg1019@gmail.com>
2020-11-11 16:42:23 -0800

    removing database info

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -2,7 +2,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// const URI2 = "mongodb://rethink:J4Y6RUZnqUmxkjVS@cluster0-shard-00-00.kmrz6.mongodb.net:27017,cluster0-shard-00-01.kmrz6.mongodb.net:27017,cluster0-shard-00-02.kmrz6.mongodb.net:27017/sample_training?ssl=true&replicaSet=atlas-n0cwc0-shard-0&authSource=admin&retryWrites=true&w=majority"
 const URI = "mongodb://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@" + process.env.DB_HOST + "/" + process.env.DB_COLLECTION + "?ssl=true&replicaSet=atlas-n0cwc0-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 const connectDB = async () => {
